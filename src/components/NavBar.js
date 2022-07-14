cbd8219d40c993da4a27ec1a6dc751a01702e68b
Vasunuri-Ganesh <108877012+Vasunuri-Ganesh@users.noreply.github.com>
2022-07-14 13:40:34 +0530

    creating navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,16 @@
+import { Link } from "react-router-dom";
 function NavBar() {
-  return <div className="">NavBar</div>;
+  return (
+    <div>
+      <ul id="nav">
+        <li>
+          <Link to="/">Home </Link>
+        </li>
+        <li>
+          <Link to="create-post">createPost </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 export default NavBar;
