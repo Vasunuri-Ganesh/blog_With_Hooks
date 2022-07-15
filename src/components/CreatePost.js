1f292a04777053711c403eb9ddf836a3088e99dc
Vasunuri-Ganesh <108877012+Vasunuri-Ganesh@users.noreply.github.com>
2022-07-15 08:45:04 +0530

    Creating post

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,4 +1,45 @@
+import { useState } from "react";
+
 function CreatePost() {
-  return <div className="">CreatePost</div>;
+  const [Title, setTitle] = useState("");
+  const [Content, setContent] = useState("");
+  const [SubTitle, setSubTitle] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("title", Title);
+  }
+
+  return (
+    <div className="create-post">
+      <h1>CreatePost</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-field">
+          <label>Title</label>
+          <input value={Title} onChange={(e) => setTitle(e.target.value)} />
+        </div>
+
+        <div className="form-field">
+          <label>SubTitle</label>
+          <input
+            value={SubTitle}
+            onChange={(e) => setSubTitle(e.target.value)}
+          />
+        </div>
+
+        <div className="form-field">
+          <label>Content</label>
+          <textarea
+            value={Content}
+            onChange={(e) => setContent(e.target.value)}
+          >
+            {" "}
+          </textarea>
+        </div>
+        <button className="create-post-btn">Create Post </button>
+      </form>
+    </div>
+  );
 }
+
 export default CreatePost;
